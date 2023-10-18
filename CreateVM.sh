98144eb4fe17c21d07724902fd727a7a52b97f72
vchiang999 <vchiang999@outlook.com>
2023-10-18 12:24:21 +0100

    add check_resource_group funtion

--- CreateVM.sh
+++ CreateVM.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-#Sample script
+#check if resource group already exists.
+check_resource_group () {
+    while true; do
+        read -p "Enter name for your Resource Group" resource_group
+        if [ $(az group exists --name $resource_group) = true ]; then
+            echo "Resource Group $resouce_group already exists"
+        else
+            break
+        fi
+    done    
+}
 
-echo Hello World!
+#create the Resource Group
+create_resource_group () {
+
+}
+
+check_resource_group
+
+#read -p "Enter name for VM" vm_name
+#read -p "Enter user name for VM" usr_name
+#read -p "Choose a region" region
