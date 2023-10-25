a836fd5eecfc6c0200530a717f5b31ccbe215924
vchiang999 <vchiang999@outlook.com>
2023-10-25 22:34:46 +0100

    improve some codes

--- LAMP-Stack/LAMP-Stack.sh
+++ LAMP-Stack/LAMP-Stack.sh
@@ -5,11 +5,11 @@ sudo apt update && sudo apt install lamp-server^ && sudo apt install unzip
 #Create info.php page
 sudo sh -c 'echo "<?php phpinfo(); ?>" > /var/www/html/info.php'
 
-echo "Download and unzip Welcome website"
+#Download and unzip Welcome website
 wget https://github.com/vchiang999/VM-on-Azure/raw/main/LAMP-Stack/SimpleWebsite.zip
 unzip SimpleWebsite.zip
 
-echo "Moving files to correct location"
+#Move files to correct location
 sudo mv ./SimpleWebsite/* /var/www/html/
 
-echo "Your Web Server is up and running. You can now connect!"
+echo -e "\n\nYour Web Server is up and running. You can now connect!\n\n"
