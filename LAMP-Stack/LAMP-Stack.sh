47102d24608c968e71b91bbcb98b18acaac7c1d4
vchiang999 <vchiang999@outlook.com>
2023-10-25 20:08:35 +0100

    Include LAMP Stack

--- LAMP-Stack/LAMP-Stack.sh
+++ LAMP-Stack/LAMP-Stack.sh
@@ -4,13 +4,21 @@ sudo apt update && sudo apt install lamp-server^
 
 echo "Displaying Apache Version"
 apache2 -v
+echo ""
+echo ""
 
 echo "Displaying MySQL version"
 mysql -V
+echo ""
+echo ""
 
 echo "Displaying PHP version"
 php -v
+echo ""
+echo ""
 
 echo "create php file"
 sudo sh -c 'echo "<?php phpinfo(); ?>" > /var/www/html/info.php'
 echo "Done"
+echo ""
+echo ""
